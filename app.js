37e5a412cfa1dd602cd88339c672fbd8e37636a3
StormKing969 <wijesinghesaj@gmail.com>
2022-01-15 11:05:56 -0500

    progress

--- app.js
+++ app.js
@@ -8,6 +8,15 @@ const port = process.env.PORT || 3000;
 
 const app = express();
 
+// Starting home page content
+const homeContent = "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Exercitationem ipsam quod sunt autem reiciendis quaerat cum magnam cupiditate temporibus, repudiandae, facilis ducimus sit aperiam, explicabo libero accusantium rem reprehenderit. Maxime."
+
+// Starting about page content
+const aboutContent = "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Exercitationem ipsam quod sunt autem reiciendis quaerat cum magnam cupiditate temporibus, repudiandae, facilis ducimus sit aperiam, explicabo libero accusantium rem reprehenderit. Maxime."
+
+// Starting contact page content
+const contactContent = "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Exercitationem ipsam quod sunt autem reiciendis quaerat cum magnam cupiditate temporibus, repudiandae, facilis ducimus sit aperiam, explicabo libero accusantium rem reprehenderit. Maxime."
+
 // needed to use EJS
 app.set("view engine", "ejs");
 
@@ -16,11 +25,20 @@ app.use(bodyParser.urlencoded({extended: true}));
 // used to style the pages
 app.use(express.static("public"))
 
-
+// Home Page
 app.get("/", function(req, res) {
-    
+    res.render("home", {homeContent: homeContent})
 })
 
+// About Page
+app.get("/about", function(req, res) {
+    res.render("about", {aboutContent: aboutContent})
+})
+
+// Contact Page
+app.get("/contact", function(req, res) {
+    res.render("contact", {contactContent: contactContent})
+})
 
 app.listen(port, function() {
     console.log("Server is connected to port " + port)
