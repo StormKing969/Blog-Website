88149e93f86f8c1d9f14fffc20552ac8656d8d27
StormKing969 <wijesinghesaj@gmail.com>
2022-01-15 09:31:21 -0500

    update

--- app.js
+++ app.js
@@ -1,10 +1,27 @@
+// jshint esversion:6
+
 const express = require("express");
-const bodyParse = require("body-parser");
+const bodyParser = require("body-parser");
 const ejs = require("ejs");
+
+const port = process.env.PORT || 3000;
 
 const app = express();
 
+// needed to use EJS
 app.set("view engine", "ejs");
 
-app.use(bodyParse.urlencoded({extended: true}));
-app.use(express.static("public"));
+// need to use "body"
+app.use(bodyParser.urlencoded({extended: true}));
+// used to style the pages
+app.use(express.static("public"))
+
+
+app.get("/", function(req, res) {
+    
+})
+
+
+app.listen(port, function() {
+    console.log("Server is connected to port " + port)
+});
